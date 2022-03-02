eb4b04ab7f18d294e237149193b11362878b4171
tienlonghungson <61400258+tienlonghungson@users.noreply.github.com>
2022-03-02 11:13:11 +0700

    add src and circuit files

---
+++ ArduinoInterrupt/ArduinoInterrupt.ino
@@ -0,0 +1,18 @@
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), 
+    blink, RISING);
+}
+
+void loop() {
+  digitalWrite(ledPin, state);
+}
+
+void blink() {
+  state = !state;
+}
